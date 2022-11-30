731c53eb549c0fe1d091400f05785a984fd225d9
raheemelassadi <raheemelassadi1@gmail.com>
2022-11-30 03:39:09 -0800

    init

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id: '1',
+        Name: 'raheem el-assadi',
+        age: '23'
+    },
+    {
+        id: '2',
+        Name: 'alexis el-assadi',
+        age: '23'
+    },
+    {
+        id: '3',
+        Name: 'dog el-assadi',
+        age: '3'
+    }
+]
+
+export default Employees
